a7ac18ae3f941023f4bd7cffd0b3ebd4f795b487
Akhil <bandariakhil88@gmail.com>
2026-04-08 11:48:07 +0530

    Refactor API key handling in frontend_agent.py
    Refactor API key retrieval and update LLM initialization.

--- agents/frontend_agent.py
+++ agents/frontend_agent.py
@@ -1,14 +1,14 @@
 import os
 import json
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from state import CodeCrafterState
 import streamlit as st
 
-load_dotenv(override=True)
 
-api_key = st.secrets["GEMINI_API_KEY_1"]
+
+api_key = st.secrets.get("GEMINI_API_KEY_1")
 
 def frontend_agent(state: CodeCrafterState) -> CodeCrafterState:
     frontend_outputs = {}
@@ -16,7 +16,7 @@ def frontend_agent(state: CodeCrafterState) -> CodeCrafterState:
     try:
         llm = ChatGoogleGenerativeAI(
             model="gemini-2.5-flash-lite",
-            google_api_key=api_key,
+            api_key=api_key,
         )
 
         services = state.get("services", [])
