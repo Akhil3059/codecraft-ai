2fd6d8a4519685ca43c37c74bc1377dbb541e0d9
Akhil <bandariakhil88@gmail.com>
2026-04-08 10:30:48 +0530

    Update API key retrieval in swagger_agent.py

--- agents/swagger_agent.py
+++ agents/swagger_agent.py
@@ -9,6 +9,7 @@ from state import CodeCrafterState, get_file_extension
 from langsmith import traceable
 
 load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY_1") or st.secrets["GEMINI_API_KEY_1"]
 @traceable(
     name="swagger_agent",
    
@@ -21,7 +22,7 @@ def swagger_agent(state: CodeCrafterState) -> CodeCrafterState:
     try:
         llm = ChatGoogleGenerativeAI(
             model = "gemini-2.5-flash-lite",
-            google_api_key = os.getenv("GEMINI_API_KEY_3")
+            google_api_key = api_key,
         )
         service_outputs = state.get("service_outputs", {})
 
